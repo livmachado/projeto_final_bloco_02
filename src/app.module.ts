c661dd61142c90f9e2886d9e1defbfbca2c2718b
Livia Machado <liviamcampos98@gmail.com>
2026-03-17 16:28:50 -0300

    🔒 Security: Implementação da Autenticação Local e JWT com o Passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { Categoria } from './categoria/entities/categoria.entity';
 import { CategoriaModule } from './categoria/categoria.module';
 import { Produto } from './produto/entities/produto.entity';
 import { ProdutoModule } from './produto/produto.module';
+import { AuthModule } from './auth/auth.module';
+import { Usuario } from './usuario/entities/usuario.entity';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
@@ -14,12 +17,14 @@ import { ProdutoModule } from './produto/produto.module';
       username: 'root',
       password: 'root',
       database: 'db_farmacia',
-      entities: [Categoria, Produto],
+      entities: [Categoria, Produto, Usuario],
       synchronize: true,
       logging: true,
     }),
     CategoriaModule,
-    ProdutoModule
+    ProdutoModule, 
+    AuthModule,
+    UsuarioModule
   ],
   controllers: [],
   providers: [],
