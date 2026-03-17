0fa659b82265397bddc9d8e9e454d8192293bc3c
Livia Machado <liviamcampos98@gmail.com>
2026-03-17 11:32:52 -0300

    ✨ New Feature: Construção do Recurso Categoria

---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,20 @@
+import { Transform, TransformFnParams } from "class-transformer";
+import { IsNotEmpty, Length } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: "tb_categorias"})
+export class Categoria {
+
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Transform(({ value }: TransformFnParams) => value?.trim())
+    @IsNotEmpty({ message: 'A Descrição é Obrigatória. '})
+    @Length(2, 1000, { message: "A Descrição deve ter entre 10 e 1000 caracteres" })
+    @Column({ length: 1000, nullable: false })
+    nome: string;
+
+    // @OneToMany( () => Product, (product) => product.category)
+    // product: Product[];
+
+}
